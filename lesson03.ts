533d98a4bac51eb8d962b888fa3a05c662f06b2e
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-11 00:34:43 +0700

    Delete lesson03.ts

--- lesson03.ts
+++
@@ -1,9 +0,0 @@
-let fst: (a: any, b: any) => any = (a, b) => b;
-
-let fst02: <T, U>(a: T, b: U) => T = (a, b) => a;
-
-let o: { n: number; xs: object[] } = { n: 1, xs: [] };
-
-console.log(fst02("tuan", 12)); // se tra ve tuan
-
-// console.log(o(12, [{ name: "tuan" }])); // sai
